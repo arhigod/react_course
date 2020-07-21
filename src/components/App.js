39f75175ea1c3bbe4f58f57739ebb90427e8c7f7
Arseni Shynkarenka <arhigodg@gmail.com>
2020-07-22 01:08:48 +0300

    task1_task2/feature: add 4 methods of component creating

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import '../../style/App.css';
+
+class ReactComponent extends React.Component {
+    render() {
+        return (
+            <h2>React.Component</h2>
+        )
+    }
+}
+class ReactPureComponent extends React.PureComponent {
+    render() {
+        return (
+            <h3>React.PureComponent</h3>
+        )
+    }
+}
+
+function getFunctionalComponent(props) {
+    return <h4>Functional Component</h4>;
+}
+
+
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="wrapper">
+                {
+                    React.createElement("h1", null, "React.createElement")
+                }
+                <ReactComponent/>
+                <ReactPureComponent/>
+                {
+                    getFunctionalComponent()
+                }
+            </div>
+        )
+    }
+}
