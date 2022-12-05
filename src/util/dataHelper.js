f7c61a7871cd4994f55edf62b493922f323feda6
arhigod <arhigodg@gmail.com>
2022-12-05 18:15:52 +0300

    change backend

--- src/util/dataHelper.js
+++ src/util/dataHelper.js
@@ -1,5 +1,7 @@
 import moviesMock from '../../mockdata/movies.json';
 
+const service = 'https://arhigod-movies-arhigod.amvera.io';
+
 export default {
     getMovies: async function ({ sorter, filter, search, offset }) {
         try {
@@ -12,26 +14,26 @@ export default {
                 filter: filter,
                 offset: offset
             };
-            const response = await fetch('https://arhigod-movies.herokuapp.com/movies?' + new URLSearchParams(params));
+            const response = await fetch(service + '/movies?' + new URLSearchParams(params));
             const data = await response.json();
             return data;
         } catch (e) {
-            return (console.log('mockdata is used'), moviesMock.slice(0, 9));
+            return (console.log('mockdata is used'), moviesMock);
         }
     },
 
     getMovie: async function (id) {
         try {
-            const response = await fetch('https://arhigod-movies.herokuapp.com/movies/' + id);
+            const response = await fetch(service + '/movies/' + id);
             const data = await response.json();
             return data;
         } catch (e) {
-            return null;
+            return (console.log('mockdata is used'), moviesMock.data[0]);
         }
     },
 
     addMovie: async function (movie) {
-        const response = await fetch('https://arhigod-movies.herokuapp.com/movies', {
+        const response = await fetch(service + '/movies', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -43,7 +45,7 @@ export default {
     },
 
     editMovie: async function (movie) {
-        const response = await fetch('https://arhigod-movies.herokuapp.com/movies', {
+        const response = await fetch(service + '/movies', {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -55,14 +57,14 @@ export default {
     },
 
     deleteMovie: async function (id) {
-        const response = await fetch('https://arhigod-movies.herokuapp.com/movies/' + id, {
+        const response = await fetch(service + '/movies/' + id, {
             method: 'DELETE'
         });
         return response;
     },
 
     resetMovies: async function () {
-        const response = await fetch('https://arhigod-movies.herokuapp.com/resetMovies');
+        const response = await fetch(service + '/resetMovies');
         return response;
     }
 };
